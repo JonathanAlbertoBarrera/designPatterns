cd898e6d9ec63ef52f36e2adca84702445bd41d0
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2025-12-02 18:45:43 -0600

    [add] generar folio 2 numeros

--- 9.StrategyPaymentMethods/src/PagoOxxo.java
+++ 9.StrategyPaymentMethods/src/PagoOxxo.java
@@ -1,7 +1,13 @@
 public class PagoOxxo implements MetodoPago {
-    
+    private double num1=0;
+    private double num2=0;
     @Override
     public void cobrar() {
         System.out.println("Pago realizado en OXXO.");
+        num1=Math.random()*100;
+        System.out.println(num1);
+        num2=Math.random()*100;
+        System.out.println(num2);
+
     }
 }
